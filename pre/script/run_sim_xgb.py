9ca51fa8f3706d279a379ec948968213ecb404d1
wpls <wplonelystar@126.com>
2017-06-06 17:50:40 +0800

    New file.

---
+++ pre/script/run_sim_xgb.py
@@ -0,0 +1,18 @@
+import prepare as pp
+import feature_construction as fc
+import modeling as md
+
+# 计时开始
+from time import time
+
+start = time()
+
+pp.prepare_dataset_all()
+fc.construct_feature()
+# md.one_hot()
+
+md.tuning_hyper_parameters_xgb_sim()
+md.predict_test_ol_xgb()
+
+print('Running complete.')
+print('\nThe total time : {0:.0f} s'.format(time() - start))
